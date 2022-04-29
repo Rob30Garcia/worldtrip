072e33f37be17e40b435ce176caaa370ce94b88c
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-04-28 21:06:20 -0400

    chore: add cities component responsive

--- src/Components/Cities/index.tsx
+++ src/Components/Cities/index.tsx
@@ -3,12 +3,7 @@ import { City } from "./City";
 
 export function Cities() {
   return (
-    <Flex
-      w="100%" 
-      px="20"
-      pb="9"
-      direction="column"
-    >
+    <>
       <Heading
         fontSize="36"
         fontWeight="medium"
@@ -18,9 +13,12 @@ export function Cities() {
       </Heading>
 
      <Grid
-      templateColumns="repeat(4, 1fr)"
+      templateColumns={["1fr", "1fr 1fr", "repeat(3, 1fr)", "repeat(4, 1fr)"]}
+      alignItems="center"
+      justifyContent="center"
       gap="12"
       py="10"
+      px={["30px", "0"]}
      >
       <City />
       <City />
@@ -28,6 +26,6 @@ export function Cities() {
       <City />
       <City />
      </Grid>
-    </Flex>
+    </>
   );
 }
